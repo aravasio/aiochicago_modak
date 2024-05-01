765712d7441b145a668e2f620650d26f6820efd4
Alejandro Ravasio <alejandroravasio@gmail.com>
2024-05-01 11:56:29 -0300

    Initial Commit

---
+++ AIoC-Modak/AIoC_ModakApp.swift
@@ -0,0 +1,32 @@
+//
+//  AIoC_ModakApp.swift
+//  AIoC-Modak
+//
+//  Created by Alejandro Ravasio on 01/05/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AIoC_ModakApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
